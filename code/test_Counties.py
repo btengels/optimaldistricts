0e7333454714cc03eeadca29a395f93252864b94
ryanwmurray <rwm22@psu.edu>
2018-01-18 21:30:18 -0500

    Fixed bug, added pseudocode to solve county regularized problem

--- code/test_Counties.py
+++ code/test_Counties.py
@@ -16,7 +16,33 @@ def linearized_County_Cost(Gamma,df):
     output = np.zeros(Gamma.shape)
     for county in county_names:
         tmp = (df['COUNTY_NAM'] == county)
+        N = np.sum(tmp)
         tmp_Gamma = np.diag(tmp).dot(Gamma)
-        avg = np.sum(tmp_Gamma,axis=0)
-        output += np.diag(tmp).dot(Gamma-avg) #This uses a funny quirk, where subtracting a vector from a matrix will execute the operation to all the rows. May need some massaging to fix, but something like this should work.
+        s = np.sum(tmp_Gamma,axis=0)
+        output += np.diag(tmp).dot(N*Gamma-s) #This uses a funny quirk, where subtracting a vector from a matrix will execute the operation to all the rows. May need some massaging to fix, but something like this should work.
     return output
+
+
+
+#The following is pseudocode for solving:
+#Given w fixed
+#Find a transportation plan from v to w
+#Which minimizes the standard transport distance plus the county distance
+
+county_param = .1
+step_size = .1
+dist_mat = get_dist_mat()
+num_it = 100
+Gamma = get_OT(v,w,dist_Mat)
+df = dataframe
+
+for i in range(num_it):
+    lin_county = linearized_County_Cost(df)
+    cost_mat = county_param*lin_county + dist_mat
+    delta_Gamma = get_OT(v,w,cost_mat)
+    Gamma = Gamma + delta_Gamma*step_size
+
+#TODO here: pull functions from make_maps to get this to work (i.e. make it not pseudocode). Then run it on PA.
+#Note: Lloyd's algorithm actually works here without any modification. This is because the county cost depends on
+#Gamma and not w, so we can do the gradient descent steps in w in exactly the same way as before (no change to code).
+
