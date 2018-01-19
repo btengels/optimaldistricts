40c4b7a686c2557bb5d82b2444be4a34cc4f091d
ryanwmurray <rwm22@psu.edu>
2018-01-19 17:10:51 -0500

    Coded out Lloyd's algorithm with the county cost. NOT tested.

--- code/test_Counties.py
+++ code/test_Counties.py
@@ -6,6 +6,39 @@
 #
 # where here g is a function mapping districts to some geographic community and Gamma_i and Gamma_j are the ith and jth rows of the transportation plan Gamma.
 
+#Included is a function which computes the linearized transport cost.
+
+
+#############################################
+#Importing necessary functions/libraries
+
+from __future__ import division
+
+import os
+from subprocess import call
+from glob import glob
+
+import numpy as np
+import pandas as pd
+import geopandas as geo
+import seaborn as sns
+import multiprocessing as mp
+import shapely
+import pickle
+
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Polygon
+
+import transport_plan_functions as tpf
+
+######################################################
+
+
+
+#######################################################
+
 #Inputs: Gamma - Current transportation plan
 #        df    - Data frame.
 
@@ -28,21 +61,71 @@ def linearized_County_Cost(Gamma,df):
 #Given w fixed
 #Find a transportation plan from v to w
 #Which minimizes the standard transport distance plus the county distance
+#I'm going to assume that the working directory is one of the data-files folders at the moment.
+
+df = pd.read_pickle('precinct_data.p')
+
+
+
 
 county_param = .1
-step_size = .1
-dist_mat = get_dist_mat()
-num_it = 100
-Gamma = get_OT(v,w,dist_Mat)
-df = dataframe
+step_size = .1 #Step size for linearized county cost steps
+reg=.5 #??
 
-for i in range(num_it):
-    lin_county = linearized_County_Cost(df)
-    cost_mat = county_param*lin_county + dist_mat
-    delta_Gamma = get_OT(v,w,cost_mat)
-    Gamma = Gamma + delta_Gamma*step_size
 
-#TODO here: pull functions from make_maps to get this to work (i.e. make it not pseudocode). Then run it on PA.
-#Note: Lloyd's algorithm actually works here without any modification. This is because the county cost depends on
-#Gamma and not w, so we can do the gradient descent steps in w in exactly the same way as before (no change to code).
 
+# weight each precinct by its population share
+n_districts = int(df.CD_2010.max()) + 1
+n_precincts = len(df)
+pcnct_loc = df[['INTPTLON10', 'INTPTLAT10']].values
+pcnct_pop = np.maximum(df.POP_TOTAL.values, 20)
+pcnct_wgt = pcnct_pop/pcnct_pop.sum()
+
+F_wgt = np.ones((n_districts,))/n_districts
+F_loc0 = np.zeros((n_districts, 3))
+for i in range(n_districts):
+
+	tmp = df[df['CD_2010'].values==i]
+
+	# df['pop_area'] = df.POP_TOTAL/(df.area*1000)
+	pop_argmax = tmp['POP_TOTAL'].argmax()
+
+	F_loc0[i, 0] = tmp['INTPTLON10'].loc[pop_argmax]
+	F_loc0[i, 1] = tmp['INTPTLAT10'].loc[pop_argmax]
+
+
+
+
+num_it = 100 #Number of iterations for the county cost
+lloyd_steps = 30
+lineSearchN = 20
+uinit = np.ones(len(I_wgt))/len(I_wgt)
+Gamma,u,cost = tpf._computeSinkhorn(pcnct_wgt,F_wgt,dist_mat,reg,uinit)
+F = F_loc0
+
+for i_step in range(1,newtonSteps):
+
+    #At each step find the the optimal transportation map Gamma given fixed F
+    dist_mat = tpf.distance_metric(I_loc,F,0)
+
+    for i in range(num_it):
+        lin_county = linearized_County_Cost(Gamma,df)
+        cost_mat = county_param*lin_county + dist_mat
+        delta_Gamma,u,cost = tpf.computeSinkhorn(pcnct_wgt,F_wgt,cost_mat,reg,u)
+        Gamma = Gamma + delta_Gamma*step_size
+
+    #Then step to the best F given fixed transportation map Gamma
+    Grad = tpf._transportGradient(pcnct_loc, pcnct_wgt, F, F_wgt, Gamma, dist_mat)
+
+    # find optimal step size (will step in direction of gradient)
+    for j in range(lineSearchN):
+    	DistMat = distance_metric(pcnct_loc, F - stepsize_vec[j]*Grad, 0)
+    	cost = np.sum(DistMat,Gamma)
+        costVec[j] = cost 
+    
+    # find the optimal step size and adjust F accordingly
+    ind = np.argmin(costVec)
+    # print(np.min(costVec))
+    F -= stepsize_vec[ind]*Grad
+
+#Next step: take output and plot. First, need to save final district to the dataframe (see line 220). Lines 414-415 in make_maps sets the colors for patches. plot_state() should make the state plot (see 399-402). A separate plot with the county boundaries would be sensible: could just reset the colors using a different field (namely 'COUNTY_NAM').
